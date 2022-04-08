8f41101f8955474a836799da61995c0142ac974e
Hamsi2007 <102812720+Hamsi2007@users.noreply.github.com>
2022-04-08 14:48:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function area()
+{
+    var n1=Number(document.getElementById("n1").value);
+    var a=n1*n1;
+    document.getElementById("n2").innerHTML=a;
+}
+function pere()
+{
+    var n1=Number(document.getElementById("n1").value);
+    var a=4*n1;
+    document.getElementById("n2").innerHTML=a;
+}
